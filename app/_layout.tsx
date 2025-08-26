858fe106550960f053ae1e72c1382eeed14a0d97
Jan Balangue <jan.a.balangue@gmail.com>
2025-08-25 19:30:26 -0700

    edit _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerStyle: { backgroundColor: '#ddd' }, headerTintColor: '#333' }}>
       <Stack.Screen name="index" options={{ title: "Home" }} />
       <Stack.Screen name="About" options={{ title: "About" }} />
       <Stack.Screen name="Contact" options={{ title: "Contact" }} />
